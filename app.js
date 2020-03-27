c2d4a4bd78c1fede2b676e28cd2926f0472146c8
Popovichenko Oksana <popovichenko.oksana@gmail.com>
2020-03-27 12:32:29 +0200

    Add pug templates

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Error page'});
+    res.status(404).render('404', {pageTitle: 'Error'});
 });
 
 app.listen(3000);
